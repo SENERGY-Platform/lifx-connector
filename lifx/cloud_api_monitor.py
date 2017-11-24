9be33b00059932e6375f06f2bc3f5521f8eb0d28
y-du <42994541+y-du@users.noreply.github.com>
2017-11-24 13:27:16 +0100

    set interval to 2min to avoid "to many requests" error

--- lifx/cloud_api_monitor.py
+++ lifx/cloud_api_monitor.py
@@ -31,7 +31,7 @@ class Monitor(Thread):
 
     def run(self):
         while True:
-            time.sleep(30)
+            time.sleep(120)
             unknown_devices = self._queryLifxCloud()
             if unknown_devices:
                 self._evaluate(unknown_devices)
