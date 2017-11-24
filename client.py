2ddeeaf37c2650a7f35712ca8cc8c63d7dc3b106
y-du <42994541+y-du@users.noreply.github.com>
2017-11-24 13:27:39 +0100

    implemented command router

--- client.py
+++ client.py
@@ -8,6 +8,7 @@ try:
     from modules.http_lib import Methods as http
     from modules.device_pool import DevicePool
     from connector.client import Client
+    from lifx.cloud_api_configuration import LIFX_CLOUD_URL, LIFX_API_KEY
     from lifx.cloud_api_monitor import Monitor
 except ImportError as ex:
     exit("{} - {}".format(__name__, ex.msg))
@@ -16,6 +17,33 @@ except ImportError as ex:
 logger = root_logger.getChild(__name__)
 
 
+def router():
+    while True:
+        task = Client.receive()
+        try:
+            for part in task.payload.get('protocol_parts'):
+                if part.get('name') == 'data':
+                    command = part.get('value')
+            http_resp = http.put(
+                '{}/lights/id:{}/state'.format(
+                    LIFX_CLOUD_URL, task.payload.get('device_url')
+                ),
+                command,
+                headers={
+                    'Authorization': 'Bearer {}'.format(LIFX_API_KEY),
+                    'Content-Type': 'application/json'
+                }
+            )
+            if http_resp.status not in (200, 207):
+                logger.error("could not route message to LIFX API - '{}'".format(http_resp.status))
+            response = str(http_resp.status)
+        except Exception as ex:
+            logger.error("error handling task - '{}'".format(ex))
+            response = '500'
+        Client.response(task, response)
+
+
 if __name__ == '__main__':
     lifx_cloud_monitor = Monitor()
     connector_client = Client(device_manager=DevicePool)
+    router()
