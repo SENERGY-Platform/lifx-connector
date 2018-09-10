e35eb6e97e8d6e65e54bee03fb5e2c912b828136
y-du <42994541+y-du@users.noreply.github.com>
2018-09-10 11:06:41 +0200

    added config parser

--- lifx/cloud_api_configuration.py
+++ lifx/cloud_api_configuration.py
@@ -1,7 +1,33 @@
 if __name__ == '__main__':
     exit('Please use "client.py"')
 
-LIFX_CLOUD_URL = 'https://api.lifx.com/v1'
-LIFX_API_KEY = 'c2be570716a71351201aeb454e43bd5a055c60f061ed3cefa8245d23cb2ce6d4'
+import os, configparser
 
-# add local config parser
+conf_path = '{}/lifx'.format(os.getcwd())
+conf_file = 'lifx.conf'
+
+config = configparser.ConfigParser()
+
+
+if not os.path.isfile(os.path.join(conf_path, conf_file)):
+    print('No config file found')
+    config['LIFX'] = {
+        'cloud_url': 'https://api.lifx.com/v1',
+        'api_key': ''
+    }
+    with open(os.path.join(conf_path, conf_file), 'w') as cf:
+        config.write(cf)
+    exit("Created blank config file at '{}'".format(conf_path))
+
+
+try:
+    config.read(os.path.join(conf_path, conf_file))
+except Exception as ex:
+    exit(ex)
+
+
+LIFX_CLOUD_URL = config['LIFX']['cloud_url']
+LIFX_API_KEY = config['LIFX']['api_key']
+
+if not LIFX_API_KEY:
+    exit('Please provide a Lifx API key')
